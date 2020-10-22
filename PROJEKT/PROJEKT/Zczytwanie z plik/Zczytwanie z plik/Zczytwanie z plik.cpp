d9814afdbeadb1c34937f9a2cd51ea3acf943b9b
Agnieszka Badocha <agnieszka.badocha@gmail.com>
2020-10-22 14:19:37 +0200

    propozycja zczytywania danych z pliku

---
+++ PROJEKT/PROJEKT/Zczytwanie z plik/Zczytwanie z plik/Zczytwanie z plik.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <fstream> 
+#include <sstream> 
+#include <vector>
+
+
+using namespace std;
+
+int main()
+{
+    ifstream plikzgrafem;
+    plikzgrafem.open("przykladowygraf.txt", ios::in);
+    if (plikzgrafem.good()==false)
+    {
+        cout << "Pilk nie istnieje";
+        return 0;
+    }
+    else
+    {
+        string linia;
+        while (getline(plikzgrafem,linia))
+        {
+            //cout << linia << endl;
+            stringstream ss;
+            ss << linia;
+            int nr_wiercholka;
+            while (ss >> nr_wiercholka) //nie pomija pustych wierszy
+            {
+                cout << nr_wiercholka << '\t';
+            }
+            cout << endl;
+        }
+    }
+   
+    return 0;
+}
+
